deb6a3499a34ef0d20fcc670bc913fdc8b63347f
FellipeNobre <fellipe11nbizarria@gmail.com>
2023-08-30 14:00:20 -0300

    Day 01

---
+++ Inicio/Aula1.js
@@ -0,0 +1,9 @@
+const nome = "fellipe"
+const sobrenome = "nobre"
+let idade = 19
+let peso = 74
+const nascimento = 2023 - idade
+let altura = 1.80
+let imc = peso /(altura * altura)
+console.log(`Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e peso ${peso} kg 
+tenho ${altura} de altura e nasci no ano ${nascimento}, altualmente tenho um IMC de ${imc} `)
---
+++ Inicio/Strings.js
@@ -0,0 +1,11 @@
+let umaString = "Fellipe"
+
+console.log(umaString[4]) //Pega um elemento do indice
+console.log(umaString.charAt(6)) //Pega um elemento do indice
+console.log(`${umaString} Nobre Bizarria`) //concatenação de Strinng 
+console.log(umaString.indexOf('elli')) // Mostra aonde começa o elemento no indice
+console.log(umaString.replace("Fellipe", "Nobre")) //Permite vc troca a palavra
+console.log(umaString.length)// Mostra o tamanho da String
+console.log(umaString.slice(2,6))// Mostra apenas as letras nesse meio do indice
+console.log(umaString.toUpperCase()) // Deixa a String toda maiuscula
+console.log(umaString.toLowerCase()) // Deixa a String toda minuscula
