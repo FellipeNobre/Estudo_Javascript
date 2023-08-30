ad605b37d4d86f4f2847abb1a183a2efd94fef1a
FellipeNobre <fellipe11nbizarria@gmail.com>
2023-08-30 15:46:30 -0300

    Day 1.1

---
+++ Inicio/number.js
@@ -0,0 +1,18 @@
+let num1 = 1
+let num2 = 2.5
+
+console.log(num1.toString() + num2)
+console.log(typeof num1) // Mostra se é numero ou string
+console.log(num1.toString(2)) //Representação binaria do número
+console.log(num2.toFixed(2)) //Arredonda o número 
+console.log(Number.isInteger(num1)) // Mostra se o num é um numero inteiro 
+
+console.log(Math.floor(num2)) // arredonda para baixo o número
+console.log(Math.ceil(num2)) // arredonda para cima o número
+console.log(Math.round(num2)) // arredonda para o mais proximo
+console.log(Math.max(1,2,3,4,5,6,7,8,9)) // pega o maior número 
+console.log(Math.min(1,2,3,4,5,6,7,8,9)) // Pega o menor número
+console.log(Math.random())
+
+
+
